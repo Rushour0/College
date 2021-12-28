eea6e3211d760d792f986ad95d90d05a7d3e22d8
Rushikesh Patade <72869428+Rushour0@users.noreply.github.com>
2021-12-28 11:26:48 +0530

    upto 8 hex places - 12!

--- MA/Code/2-factorial.asm
+++ MA/Code/2-factorial.asm
@@ -1,65 +1,87 @@
-; macro to perform system call
-%macro RW 4
-    mov rax, %1
-    mov rdi, %2
-    mov rsi, %3
-    mov rdx, %4
-    syscall
+;Macro For Syscalls Print,Read,Exit
+%macro PR 4
+mov rax,%1
+mov rdi,%2
+mov rsi,%3
+mov rdx,%4
+syscall
 %endmacro
 
-; Section data to store
 section .data
-    msg db 'Enter a number:', 32
-    msg_len equ $-msg
-    msg1 db 'The Factorial of the number is : ', 32
-    msg1_len equ $-msg1
 
-; Section bss to reserve memory space
+msg1 db 10,"______Factorial______"
+msg1len equ $-msg1
+
+msg2 db 10,"Enter A number between 00H-FFH : "
+msg2len equ $-msg2
+
+newline db 10
+
+num1 dd 0
+
 section .bss
-    temp resb 1
-    temp1 resb 1
-    num1 resw 10
 
-; Section to get things started
+num2 resb 1
+
+fact resd 1
+temp1 resd 1
+temp2 resb 1
+extra resd 1
+
+temp3 resb 1
+
 section .text
-    global _start
-    
-    _start:
-        mov bp, 4
-        RW 1, 1, msg, msg_len                       ; display msg
-        RW 0, 0, num1, 10                           ; take input into num1
-        RW 1, 1, msg1, msg1_len                     ; display msg1
-        mov al, byte[num1]                          ; move data in num1 to al register
-        cmp al, 39h                                 ; compare with 39H to check what number it is          
-        jbe next                                    ; jump if below or equal to 39H
-        sub al, 07h                                 ; converting ascii number to hex number
 
-    next:
-        sub al, 30h                                 ; converting ascii number to hex number
-        mov word[num1], ax                          ; store al register value in num1
-        mov ax, 01h
-        
-    factorial:
-        mul word[num1]                              ; multiply num1 with ax   
-        dec word[num1]                              ; decrease num1 by 1
-        cmp word[num1], 0000h                       ; check if num1 has become 0
-        jnz factorial                               ; jump to factorial if num1 not zero
-    
-    ; code to display two characters in the al register
-    up: 
-        rol ax, 4       
-        mov bx, ax
-        and al, 0fh
-        cmp al, 09h
-        jbe down
-        add al, 07
-    
-    down:
-        add al, 30h
-        mov byte[temp1], al
-        RW 1, 1, temp1, 1
-        mov ax, bx
-        dec bp
-        jnz up
-    
-    RW 60,0,0,0                                     ; system end call
+global _start
+_start:
+
+;Printing of messages and reading inout for number
+PR 1,1,msg1,msg1len
+PR 1,1,msg2,msg2len
+
+PR 0,0,num2,1
+PR 1,1,num2,1
+PR 1,1,newline,1
+
+;Conversion of Ascii to hex
+cmp byte[num2],40h
+jbe DOWN2
+sub byte[num2],07h
+DOWN2:sub byte[num2],30h
+
+mov al,byte[num2]
+add byte[num1],al
+
+;Resetting eax to zero
+mov eax,00000000h
+
+;Loop For multiplying the number in eax
+mov eax,dword[num1]
+UP1:dec dword[num1]
+mul dword[num1]
+cmp word[num1],1
+ja UP1
+mov dword[fact],eax
+
+mov eax,dword[fact]
+
+;Loop to print the contents of EAX after ascii conversion
+mov bp,8
+UP3:rol eax,4
+mov dword[temp1],eax
+and al,0fh
+cmp al,09h
+jbe down1
+add al,07h
+down1: add al,30h
+mov byte[temp2],al
+PR 1,1,temp2,1
+mov eax,dword[temp1]
+dec bp
+jnz UP3
+
+;End
+PR 1,1,newline,1
+PR 60,0,0,0
+
+
